654b8a65daec2cd04ca07ebcd90dcbb150579a39
abautist <auggie.bautista@gmail.com>
2016-02-12 09:56:12 -0400

    organized script into usage class

--- test.rb
+++ test.rb
@@ -1,106 +1,88 @@
 require 'logger'
 
-# Global variables
-cpu_threshold = 80  # %age of cpu that we should be alerting at
-cpu_tries = 5
-cpu_wait = 30
-
-memory_threshold = 80 # %age of memory that we should be alerting at
-memory_tries = 5
-memory_wait = 30
-
 $pagerduty_api_key = ENV['PAGERDUTY_API_KEY']
 
-logger = Logger.new("| tee test.log")
-logger.level = Logger::INFO
+class Usage
+  def initialize 
+    @threshold = 80
+    @tries = 5
+    @wait = 30
+    @logger = Logger.new("| tee test.log")
+    @logger.level = Logger::INFO
 
-def get_cpu
-  # Get current cpu usage - let's check usage and iowait
-  # TODO: we want to check all cpu states
-  system "top -l 1 | grep 'CPU usage' > ./sample.txt"
+    system "top -l 1 | grep 'CPU usage' > ./sample.txt"
+    file = File.open("./sample.txt", "rb").read
+    file_arr = file.split(" ")
+    @cpu_usage = file_arr[4].to_f
 
-  file = File.open("./sample.txt", "rb").read
-  file_arr = file.split(" ")
-
-  cpu_usage = file_arr[4]
-
-  return cpu_usage.to_f
-end
-
-def getmemory
-  # Get memory usage
-  system "top -l 1 | grep PhysMem > ./memory.txt"
-
-  file = File.open("./memory.txt", "rb").read
-  file_arr = file.split(" ")
-  # print file_arr
-
-  used = file_arr[1].to_f
-  # print used
-  total = file_arr[5].to_f + used
-  # print total
-
-  return (used / total) * 100
-end
-
-logger.info("Checking cpu utilization")
-cpu_usage = get_cpu()
-
-# If the cpu is over the threshold, then keep checking
-if cpu_usage > cpu_threshold
-  logger.info("CPU is over threshold, continuing to check")
-  cpu_over = 1
-
-  for i in 0..cpu_tries
-    logger.info("CPU check try #{i}")
-    if(get_cpu > cpu_threshold)
-      cpu_over = cpu_over + 1
-    end
-    logger.info("Sleeping for #{cpu_wait} seconds")
-    sleep(cpu_wait)
+    system "top -l 1 | grep PhysMem > ./memory.txt"
+    file = File.open("./memory.txt", "rb").read
+    file_arr = file.split(" ")
+    used = file_arr[1].to_f
+    total = file_arr[5].to_f + used
+    @mem_usage = (used / total) * 100
   end
 
-    if cpu_over > cpu_tries
-      # Let's alert on this
-      logger.info("CPU is over, alerting via pagerduty")
-      pagerduty = Pagerduty.new($pagerduty_api_key)
-      incident = pagerduty.trigger("CPU is high: #{cpu_usage}")
+  def check_cpu
+    @logger.info("Checking cpu utilization")
+
+    if @cpu_usage > @threshold
+      @logger.info("CPU is over threshold, continuing to check")
+      @cpu_over = 1
+
+      (0..@tries).each do |i|
+        @logger.info("CPU check try #{i}")
+        if @cpu_usage > @threshold
+          @cpu_over += 1
+        end
+        @logger.info("Sleeping for #{@wait} seconds")
+        sleep(@wait)
+      end
+
+      if @cpu_over > @tries
+        @logger.info("CPU is over, alerting via pagerduty")
+        pagerduty = Pagerduty.new($pagerduty_api_key)
+        incident = pagerduty.trigger("CPU is high: #{@cpu_usage}")
+      end
     end
-end
-
-# memory checks
-logger.info("Checking memory")
-mem_usage = getmemory()
-
-if mem_usage > memory_threshold
-   logger.info("Mem is over threshold, continuing to check")
-  mem_over = 1
-
-  for i in 0..memory_tries
-    logger.info("MEM check try #{i}")
-    if(getmemory > memory_threshold)
-      mem_over = mem_over + 1
-    end
-   logger.info("Sleeping for #{cpu_wait} seconds")
-    sleep(memory_wait)
   end
 
-  if mem_over > memory_tries
-    # Let's alert on this
-    logger.info("mem is over, alerting via pagerduty")
-    pagerduty = Pagerduty.new($pagerduty_api_key)
+  def check_memory
+    @logger.info("Checking memory")
 
-    begin
-      require 'sys-uname'
-      incident = pagerduty.trigger(
-        "Mem is high: #{mem_usage}",
-         client:       Sys::Uname.nodename,
-      )
-    rescue Net::HTTPServerException => error
-     puts "PAGERDUTY FAILED!"
-      puts error.response.code
-      puts error.response.message
-      puts error.response.body
+    if @mem_usage > @threshold
+      @logger.info("Mem is over threshold, continuing to check")
+      @mem_over = 1
+
+      (0..@tries).each do |i|
+        @logger.info("MEM check try #{i}")
+        if @mem_usage > @threshold
+          @mem_over += 1
+        end
+        @logger.info("Sleeping for #{@wait} seconds")
+        sleep(@wait)
+      end
+
+      if @mem_over > @tries
+        @logger.info("mem is over, alerting via pagerduty")
+        pagerduty = Pagerduty.new($pagerduty_api_key)
+        begin
+          require 'sys-uname'
+          incident = pagerduty.trigger(
+            "Mem is high: #{mem_usage}",
+             client:       Sys::Uname.nodename,
+          )
+        rescue Net::HTTPServerException => error
+          puts "PAGERDUTY FAILED!"
+          puts error.response.code
+          puts error.response.message
+          puts error.response.body
+        end
+      end
     end
   end
 end
+
+usage = Usage.new()
+puts usage.check_cpu
+puts usage.check_memory
